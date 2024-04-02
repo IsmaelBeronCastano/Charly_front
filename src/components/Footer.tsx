ade8e78249d81e2f1f5b4ca37e5909eed136ed85
Ismael Berón <portafolioismaelberon@gmail.com>
2024-04-03 00:02:46 +0200

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+
 
 const Footer = () => {
   return (
     <div className="bg-purple-600 p-8">
-        <h3 className="text-white text-lg text-center">API creada y diseñada por Ismael Berón Castaño con asistencia de inteligencia artificial</h3>
+        <h3 className="text-white text-lg text-center">API creada y diseñada por Ismael Berón Castaño 2024</h3>
 
     </div>
   )
