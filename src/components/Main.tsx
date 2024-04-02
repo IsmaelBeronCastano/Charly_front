3b925c1efbe08761654e8c595fdc0fbf7776a914
Ismael Berón <portafolioismaelberon@gmail.com>
2024-04-03 00:53:28 +0200

    front

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,8 +4,8 @@ import NavigationResult from './NavigationResult'
 const Main = () => {
   return (
     <div className="flex flex-col md:flex-row">
-        <img className="w-full md:w-1/2 pl-4" src={Imagen} alt="charly portada clics modernos" />
-        <section className="w-full md:w-1/2 mb-10 pl-4">
+        <img className="w-full md:w-1/2 pl-1" src={Imagen} alt="charly portada clics modernos" />
+        <section className="w-full md:w-1/2 mb-10 pl-1">
             <p className="text-xl font-bold mt-3 mb-2">Esta es una <span className="text-purple-400">API</span> donde poder consultar parte de la discografía de <span className="text-purple-600">Charly</span> en solitario</p>
             <p className="text-xl font-bold mt-3 mb-2">Desde Yendo de la cama al living hasta Random</p>
             <NavigationResult />
