5788410a29d592e05cb8ffb25a7772deb0025508
Ismael Berón <portafolioismaelberon@gmail.com>
2024-04-03 02:07:16 +0200

    api2

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -11,7 +11,7 @@ const Main = () => {
             <NavigationResult />
             <div className="mt-6">
             <p className="text-xl font-bold mt-3 mb-2">El uso es sencillo, añadir al endpoint: </p>
-            <p><span className="text-xl">https://charly-api.up.railway.app/api/v2/discos</span></p> 
+            <p><span className="text-xl">https://charly-api2.up.railway.app/api/v2/discos</span></p> 
              <p className="mt-4"><span>Para el álbum: </span>/album/NombreDelAlbumEnPascalCase</p>
              <p><span>Para la fecha: </span>/fecha/1996</p>
              <p><span>Para la canción: </span>/cancion/TituloDeLaCancionEnPascalCase</p>
