80e587853ce8cb158379e4e4753d28910517cee9
Ismael Berón <portafolioismaelberon@gmail.com>
2024-04-03 11:29:06 +0200

    fixed!

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -10,12 +10,14 @@ const Main = () => {
                 <p className="text-xl font-bold mt-3 mb-2 pl-1">Esta es una <span className="text-purple-400">API</span> donde poder consultar parte de la discografía de <span className="text-purple-600">Charly</span> en solitario</p>
                 <p className="text-xl font-bold mt-3 mb-2 pl-1">Desde Yendo de la cama al living hasta Random</p>
                 <NavigationResult />
-                <div id="api" className="mt-6 flex flex-col justify-center items-center">
+                <div id="api" className="mt-6 flex flex-col items-center justify-center">
+                  <div className="text-left">
                     <p className="text-xl font-bold mt-3 mb-2">El uso es sencillo, añadir al endpoint: </p>
                     <p className="text-xl">https://charly-api2.up.railway.app/api/v2/discos</p> 
                     <p className="mt-4"><span>Para el álbum: </span>/album/NombreDelAlbumEnPascalCase</p>
                     <p><span>Para la fecha: </span>/fecha/1996</p>
                     <p><span>Para la canción: </span>/cancion/TituloDeLaCancionEnPascalCase</p>
+                  </div>
                 </div>
             </div>
         </section>
