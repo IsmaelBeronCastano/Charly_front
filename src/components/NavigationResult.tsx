8d81a4c281f3ebfa3d994b67cb614b2f33c9bf70
Ismael Berón <portafolioismaelberon@gmail.com>
2024-04-03 12:56:33 +0200

    height del pre, margins y paddings fuera

--- src/components/NavigationResult.tsx
+++ src/components/NavigationResult.tsx
@@ -19,7 +19,7 @@ const NavigationResult = () => {
     return (
       <div className="w-[450px]">
       <NavigationBar onSelect={fetchData} />
-      <div className="w-full m-4 p-4 border rounded max-w-full" style={{ maxHeight: '500px', overflow: 'hidden' }}>
+      <div className="w-full border rounded max-w-full" style={{ maxHeight: '500px'}}>
         <pre className="whitespace-pre-wrap break-words overflow-auto max-h-[400px]" style={{ maxHeight: '400px', overflowY: 'scroll' }}>{JSON.stringify(data, null, 2)}</pre>
       </div>
     </div>
