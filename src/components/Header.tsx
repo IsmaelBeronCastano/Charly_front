27b5890c2cbec0c51332279a02f95df96cb8c684
Ismael Berón <portafolioismaelberon@gmail.com>
2024-04-04 15:23:20 +0200

    el mas cuerdo es el mas delirante

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
             <h1 className="text-purple-600 font-bold text-6xl text-center mt-8">CHARLY GARCÍA 
             <span className="text-purple-300 text-4xl">API</span></h1>
 
-            <h3 className="text-end font-bold mt-1 mr-8 text-xl mb-3">React con Typescript, Nest y MongoDB</h3>
+            <h3 className="text-end font-bold mt-1 mr-8 text-xl mb-3 italic">"El más cuerdo es el más delirante"</h3>
     </div>
   )
 }
