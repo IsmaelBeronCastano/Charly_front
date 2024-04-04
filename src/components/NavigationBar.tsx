85aeb0af254e3e36c71331e9930d91a29ad2b069
Ismael Berón <portafolioismaelberon@gmail.com>
2024-04-04 14:00:51 +0200

    cuchillos

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -9,7 +9,7 @@ const NavigationBar = ({onSelect}: Props) => {
         <div className="bg-purple-800 text-white p-4 flex max-w-[100%] justify-between rounded">
           <button onClick={() => onSelect('album/YendoDeLaCamaAlLiving')} className="hover:bg-gray-700 rounded p-2"><span>Álbum</span></button>
           <button onClick={() => onSelect('fecha/1996')} className="hover:bg-gray-700 rounded p-2"><span>Fecha de publicación</span></button>
-          <button onClick={() => onSelect('cancion/Zocacola')} className="hover:bg-gray-700 rounded p-2"><span>Canción</span></button>
+          <button onClick={() => onSelect('cancion/Cuchillos')} className="hover:bg-gray-700 rounded p-2"><span>Canción</span></button>
         </div>
       );
 }
