0ac3e227a22fed9f822b1a11d34702154fd65d1b
Ismael Berón <portafolioismaelberon@gmail.com>
2024-04-05 19:59:20 +0200

    3 endpoints de ejemplo

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -6,10 +6,10 @@ interface Props{
 
 const NavigationBar = ({onSelect}: Props) => {
     return (
-        <div className="bg-purple-800 text-white p-4 flex max-w-[100%] justify-between rounded">
+        <div className="bg-purple-800 text-white p-4 flex max-w-[100%] justify-between rounded flex-wrap">
           <button onClick={() => onSelect('album/YendoDeLaCamaAlLiving')} className="hover:bg-gray-700 rounded p-2"><span>Álbum</span></button>
-          <button onClick={() => onSelect('fecha/1996')} className="hover:bg-gray-700 rounded p-2"><span>Fecha de publicación</span></button>
-          <button onClick={() => onSelect('cancion/Cuchillos')} className="hover:bg-gray-700 rounded p-2"><span>Canción</span></button>
+          <button onClick={() => onSelect('fecha/1996')} className="hover:bg-gray-700 rounded p-2"><span>Fecha</span></button>
+          <button onClick={() => onSelect('cancion/Cuchillos')} className="hover:bg-gray-700 rounded p-2"><span>Canción</span></button>         
         </div>
       );
 }
